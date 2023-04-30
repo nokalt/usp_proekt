1c92f828258876c4a9486e31ea1fe8c5f6d899c0
Kristiyan Mihalev <49516199+kristiyanmv@users.noreply.github.com>
2023-04-30 16:51:46 +0300

    added login/signup forms

---
+++ vaccination_proj/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vaccination_proj
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            signin_form signin_Form = new signin_form();
+           this.Hide();
+            signin_Form.Show();
+        }
+    }
+}
---
+++ vaccination_proj/signin_form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace vaccination_proj
+{
+    public partial class signin_form : Form
+    {
+        public signin_form()
+        {
+            InitializeComponent();
+        }
+    }
+}
